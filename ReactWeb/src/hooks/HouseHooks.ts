7953840f2584ab5ee53d456085354dbb9e035595
Shem Manyu <smanyu48@gmail.com>
2024-10-24 08:48:40 +0300

    Added optmizations

---
+++ ReactWeb/src/hooks/HouseHooks.ts
@@ -0,0 +1,14 @@
+import config from "../config";
+import { House } from "../types/house";
+import axios, { AxiosError } from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+const useFetchHouses = () => {
+    return useQuery<House[], AxiosError>({
+      queryKey: ["houses"],
+      queryFn: () =>
+        axios.get(`${config.baseApiUrl}/houses`).then((resp) => resp.data),
+    });
+  };
+
+export default useFetchHouses;
