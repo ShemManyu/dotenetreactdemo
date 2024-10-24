10799ed4dfb0a4aead66569d25589b4640ac9df4
Shem Manyu <smanyu48@gmail.com>
2024-10-24 08:20:25 +0300

    hooked up frontend to API

--- Api/Data/HouseDbContext.cs
+++ Api/Data/HouseDbContext.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 public class HouseDbContext : DbContext
 {
+    public HouseDbContext(DbContextOptions<HouseDbContext> o):
+        base(o) {}
     public DbSet<HouseEntity> Houses => Set<HouseEntity>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4baef7cc51f0aca0c81c88552548ddcc309223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e888cebf6a0d1f7fb06f8bf067f286d513779959")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
